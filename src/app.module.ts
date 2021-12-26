bb18271ede3a0400f2f9c286014d79eb5d7c7076
Luis Vallespin <lvallespinabad@gmail.com>
2021-12-26 03:01:34 +0100

    build: preparing the dev env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+const env = process.env;
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      `mongodb+srv://${env.DATABASE_USER}:${env.DATABASE_PASSWORD}@${env.DATABASE_URL}/${env.DATABASE_NAME}?retryWrites=true&w=majority`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
